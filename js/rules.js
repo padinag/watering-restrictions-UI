cb32501cb064690e93d2c4b3042b0575de2c7af9
padinag <adina@mini-box.com>
2017-03-21 14:54:32 +0200

    createForm

--- js/rules.js
+++ js/rules.js
@@ -1,16 +1,5 @@
 function createForm(){
     console.log("created");
-    var company= {
-        name: ""
-        phone: "",
-        country: "",
-        state: "",
-        city: "",
-        geoarea: []
-        }
-    
-    
-    
-    
 }
+ 
 

f114d2956d2729b7dc4e936a7e39a88991dc5147
padinag <adina@mini-box.com>
2017-03-21 13:55:20 +0200

    Add files via upload

---
+++ rules.js
@@ -0,0 +1,16 @@
+function createForm(){
+    console.log("created");
+    var company= {
+        name: ""
+        phone: "",
+        country: "",
+        state: "",
+        city: "",
+        geoarea: []
+        }
+    
+    
+    
+    
+}
+

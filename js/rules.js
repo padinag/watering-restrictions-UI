5e2626dc30b79357cf099f8fcbe2f70fe638fe62
padinag <adina@mini-box.com>
2017-03-22 14:37:28 +0200

    stage part

--- js/rules.js
+++ js/rules.js
@@ -1,5 +1,8 @@
 function createForm(){
     console.log("created");
+    var partOfStage=document.getElementById("stagePart");
+    var stageName = partOfStage.options[partOfStage.selectedIndex].value;
+    console.log("stageName")
 }
  
 

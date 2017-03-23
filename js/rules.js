4abbdf253d0128bad4dc9a2c7cab66fe1d612e3f
padinag <adina@mini-box.com>
2017-03-23 21:28:04 +0200

    more readings added

--- js/rules.js
+++ js/rules.js
@@ -1,5 +1,23 @@
-function createForm(){
+function readForm(){
     console.log("created");
+    var companyDetails={};
+    var name = document.getElementById("companyName").value;
+    var phone = document.getElementById("companyPhone").value;
+    var address = document.getElementById("companyAddress").value;
+    var city = document.getElementById("companyCity").value;
+    var country = document.getElementById("companyCountry").value;
+    var state = document.getElementById("companyState").value;
+    console.log(name, phone, address, city, country, state);
+    
+    function checkGeoarea(){
+        var geoareaChecked = document.getElementById("geoarea");
+        if (geoareaChecked.checked){
+            var geoareaCoord = [];
+        }
+    }
+    
+    
+    
     var partOfStage=document.getElementById("stagePart");
     var stageName = partOfStage.options[partOfStage.selectedIndex].value;
     console.log(stageName)

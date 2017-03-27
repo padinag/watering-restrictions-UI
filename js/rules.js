088d484e182a8e5676859d231e85019a3cb858c5
padinag <adina@mini-box.com>
2017-03-27 17:32:19 +0300

    added geoarea

--- js/rules.js
+++ js/rules.js
@@ -1,13 +1,7 @@
-    function checkGeoarea(){
-        var geoareaChecked = document.getElementById("geoarea").value;
-        if (geoareaChecked.checked){
-          console.log("geoarea selected");
-         }
-    }
-
-
 function readForm(){
     console.log("created");
+    
+//company details
     var companyDetails={};
     var name = document.getElementById("companyName").value;
     var phone = document.getElementById("companyPhone").value;
@@ -16,8 +10,17 @@ function readForm(){
     var country = document.getElementById("companyCountry").value;
     var state = document.getElementById("companyState").value;
     console.log(name, phone, address, city, country, state);
-    checkGeoarea();
-
+    
+//geoarea  
+    var geoareaChecked = document.getElementById("geoarea");
+        if (geoareaChecked.checked){
+          console.log("geoarea selected");
+         }
+    
+//water restrictions valid from
+    var date = document.getElementById("dp6").value;
+    console.log(date);
+    
     
     
     var partOfStage=document.getElementById("stagePart");
